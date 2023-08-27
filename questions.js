14a206adc8b6c49de6191fcc29401765b7bb34cb
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-08-28 00:15:51 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,25 @@
+//Chapter1 -question 1
+let a="harry";
+let b=6;//this will word as string
+console.log(a+b);
+//question2
+console.log(typeof(a+b))
+
+console.log(b*a)
+console.log(typeof(b+a))
+
+//question 3
+const c={
+    name:"raj",
+    section:1,
+    isprinciple:false
+
+}
+//question 4
+ c['friend']="shubham"
+ c['friend']="ramji"
+
+ console.log(c);
+//question 5
+
+ 
