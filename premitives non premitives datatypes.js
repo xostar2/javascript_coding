e35d22adf6ca57706a8cb86e64b337018d97e643
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-08-27 23:55:51 +0530

    premitives_non_premitives_datatypes.js

---
+++ premitives non premitives datatypes.js
@@ -0,0 +1,23 @@
+//premative datatypes
+
+let a=null;
+let b=345;
+let c=true;
+
+let d=BigInt("567");
+let e="harry";
+let f=Symbol("i am a nice boy")
+let g=undefined
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof(e));
+
+//  object in javascript or non premative datatype
+
+const item={
+    "harry":true,
+    "subh":false,
+    "Lovish":67,
+    "Rohan":undefined
+}
+console.log(item["harry"])
