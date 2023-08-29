2656a748ed6d1013194a9b9d3521f2b9c42e6c7c
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-08-29 10:30:31 +0530

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,9 @@
+//comparetor operator///
+console.log("Comparision operator")
+let a=6;
+let b="6";
+
+console.log("a==b",a==b)
+console.log("a!=b",a!=b)
+console.log("a!==b",a!==b)
+console.log("a===b",a===b)
