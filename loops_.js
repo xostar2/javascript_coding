50345ddbe8d6083a632aec9b36cc67f2946293e5
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-09-02 15:13:27 +0530

    Create loops_.js

---
+++ loops_.js
@@ -0,0 +1,28 @@
+//----learning loops in javascript---------
+
+// for(let i=0;i<5;i++){
+// 	console.log(i);
+// }
+
+let obj={
+	harry:95,
+	ram:99,
+	shyam:90,
+	mohit:56
+}
+
+
+//-----------for-in loop-------------------
+for(let a in obj){
+	console.log(a);
+}
+
+for(let a in obj){
+	console.log(obj[a]);
+}
+//-------for-of  loop-----------------------
+for(let b of "harry"){
+	console.log(b);
+}
+
+
