6626daf0ec7e16852fc761e23e7ae280eb8ae1d4
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-09-02 15:11:35 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,20 @@
+//----------funtion in java script--------------
+
+function addtwonumber(a,b){
+	return a+b;
+}
+
+let a=addtwonumber(2,4);
+console.log(a);
+
+const sum=(p,q)=>{
+    return p+q;
+}
+console.log(sum(3,5));
+
+console.log(3+3);
+console.log(3-"4"+3+3);
+console.log(3+3+"3"-"3");
+console.log(5-"4");
+console.log("3"+3+3);
+console.log(("3"--"3"))
