faabdb8d3d2c965e9d427171b9b6b3fdcb87649d
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-09-09 11:26:40 +0530

    Create Introduction_to_Strings.js

---
+++ Introduction_to_Strings.js
@@ -0,0 +1,23 @@
+//========STRING++++++++++++++++++
+
+let name ="harry"
+console.log(name.length)
+console.log(name);
+console.log(name[0])
+
+//=======template Literals=========
+
+let boy1="pramod"
+let boy2="nikhil"
+
+//nikhil is a friend of pramode
+// we use backtick here for template litreals
+//we can use double cots "",'', varible in that ,
+//it is call string interpolation
+let sentence=`${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+//escape sequence character
+
+let fruit='banana\'g'
+console.log(fruit)
